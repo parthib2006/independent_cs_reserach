93575c3c5aa7429796508ea5daf87bbe1eb5ec34
parthib2006 <parthibghosh6@gmail.com>
2025-10-23 22:24:06 +0530

    Day 2 : Operators and Expressions

---
+++ Section_B/C++/Cisco_netAcad/LAB_chars.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	bool answer;
+	int value;
+	
+	cout << "Enter a value: ";
+	cin >> value;
+	
+	if ((value>=0 && value<10) || (value*2 < 20 && value-2 > -2) || (value-1 > 1 && value/2 < 10) || (value==111)){
+        answer=true;
+    }
+    else{
+        answer=false;
+    } 
+	
+	cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(") << endl;
+	return 0;
+}
