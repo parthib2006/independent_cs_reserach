41acab7390a4b1b053f6b0306812226b26f4f092
parthib2006 <parthibghosh6@gmail.com>
2025-10-23 21:59:36 +0530

    Day 2 : Operators and Expressions

---
+++ Section_B/C++/Cisco_netAcad/LAB_ints.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int i, j, k;
+	
+	cout << "Enter i: ";
+	cin >> i;
+	cout << "Enter j: ";
+	cin >> j;
+	
+	i += 2;
+	j -= i;
+	k = i/j;
+	k += k;
+	k--;
+	j = k%i;
+	k += (k+i);
+	k += (k/j);
+    k = k*k*k;
+	k += i*j;
+	
+	cout << k << endl;
+	return 0;
+}
